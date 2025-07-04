428be774aafbc2bffe5495dca1fb418a9aa46e62
cefqrn <cefqrn@gmail.com>
2025-07-04 00:26:16 -0400

    replaced classes with functions

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 import automaton.Cell;
 import automaton.ElementaryCellularAutomaton;
+import automaton.ElementaryCellularAutomaton.Window;
 
 public class Main {
   public static final int ITERATIONS = 100;
 
   public static void main(String[] args) {
-    ElementaryCellularAutomaton automaton = new Rule110(ITERATIONS);
+    var automaton = new ElementaryCellularAutomaton(Main::rule110, ITERATIONS);
     automaton.cells[ITERATIONS-1] = new Cell.Alive();
 
     System.out.println(Cell.toString(automaton.cells));
@@ -16,7 +17,7 @@ public class Main {
 
     System.out.println();
 
-    automaton = new Rule124(ITERATIONS);
+    automaton = new ElementaryCellularAutomaton(Main::rule124, ITERATIONS);
     automaton.cells[0] = new Cell.Alive();
 
     System.out.println(Cell.toString(automaton.cells));
@@ -24,5 +25,31 @@ public class Main {
       automaton.step();
       System.out.println(Cell.toString(automaton.cells));
     }
+  }
+
+  private static Cell rule110(Window window) {
+    return switch (window) {
+      case Window(Cell.Alive a, Cell.Alive b, Cell.Alive c) -> Cell.DEAD;
+      case Window(Cell.Alive a, Cell.Alive b, Cell.Dead  c) -> Cell.ALIVE;
+      case Window(Cell.Alive a, Cell.Dead  b, Cell.Alive c) -> Cell.ALIVE;
+      case Window(Cell.Alive a, Cell.Dead  b, Cell.Dead  c) -> Cell.DEAD;
+      case Window(Cell.Dead  a, Cell.Alive b, Cell.Alive c) -> Cell.ALIVE;
+      case Window(Cell.Dead  a, Cell.Alive b, Cell.Dead  c) -> Cell.ALIVE;
+      case Window(Cell.Dead  a, Cell.Dead  b, Cell.Alive c) -> Cell.ALIVE;
+      case Window(Cell.Dead  a, Cell.Dead  b, Cell.Dead  c) -> Cell.DEAD;
+    };
+  }
+
+  private static Cell rule124(Window window) {
+    return switch (window) {
+      case Window(Cell.Alive a, Cell.Alive b, Cell.Alive c) -> Cell.DEAD;
+      case Window(Cell.Alive a, Cell.Alive b, Cell.Dead  c) -> Cell.ALIVE;
+      case Window(Cell.Alive a, Cell.Dead  b, Cell.Alive c) -> Cell.ALIVE;
+      case Window(Cell.Alive a, Cell.Dead  b, Cell.Dead  c) -> Cell.ALIVE;
+      case Window(Cell.Dead  a, Cell.Alive b, Cell.Alive c) -> Cell.ALIVE;
+      case Window(Cell.Dead  a, Cell.Alive b, Cell.Dead  c) -> Cell.ALIVE;
+      case Window(Cell.Dead  a, Cell.Dead  b, Cell.Alive c) -> Cell.DEAD;
+      case Window(Cell.Dead  a, Cell.Dead  b, Cell.Dead  c) -> Cell.DEAD;
+    };
   }
 }
--- src/automaton/ElementaryCellularAutomaton.java
+++ src/automaton/ElementaryCellularAutomaton.java
@@ -1,33 +1,37 @@
 package automaton;
 
 import java.util.Arrays;
+import java.util.function.Function;
 
-public abstract class ElementaryCellularAutomaton {
+public class ElementaryCellularAutomaton {
+  public Function<Window, Cell> step;
+  public Cell defaultState;
   public Cell[] cells;
 
-  protected ElementaryCellularAutomaton(int cellCount) {
-    cells = new Cell[cellCount];
-    Arrays.fill(cells, defaultState());
+  public ElementaryCellularAutomaton(Function<Window, Cell> step, int cellCount, Cell defaultState) {
+    this.defaultState = defaultState;
+    this.step = step;
+
+    this.cells = new Cell[cellCount];
+    Arrays.fill(this.cells, defaultState);
   }
 
-  public abstract Cell step(Window window);
-
-  public Cell defaultState() {
-    return Cell.DEAD;
+  public ElementaryCellularAutomaton(Function<Window, Cell> step, int cellCount) {
+    this(step, cellCount, Cell.DEAD);
   }
 
   public final Cell step(Cell left, Cell middle, Cell right) {
-    return step(new Window(left, middle, right));
+    return step.apply(new Window(left, middle, right));
   }
 
   public final ElementaryCellularAutomaton step() {
-    var previous = defaultState();
+    var previous = defaultState;
 
     for (var i=0; i < cells.length-1; ++i)
       cells[i] = step(previous, previous=cells[i], cells[i+1]);
 
     if (cells.length > 0)
-      cells[cells.length-1] = step(previous, cells[cells.length-1], defaultState());
+      cells[cells.length-1] = step(previous, cells[cells.length-1], defaultState);
 
     return this;
   }
